729fa1adaa0f124aef8cb79228881af7054610ad
thomasstorm <thomas_storm@brockmann-consult.de>
2014-10-28 10:40:39 +0100

    added <clearDB> flag to config deployment script

--- deploy_config.sh
+++ deploy_config.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-if [ $# != 4 ]; then
-    echo "Usage: deploy_config <host> <thredds_url> <openid_rp_url> <debug_flag>"
+if [ $# != 5 ]; then
+    echo "Usage: deploy_config <host> <thredds_url> <openid_rp_url> <debug_flag> <clear_db>"
     echo "    <host>            Target host"
     echo "    <thredds_url>     Base URL of thredds server being used, such as http://portal.waqss.de"
     echo "    <openid_rp_url>   URL of discovery service of OpenID server. Example: http://opec-portal-test2:8585/openid-server/provider/discovery/gis-portal"
     echo "    <debug_flag>      Debug mode if flag is 1, no debug mode otherwise"
+    echo "    <clear_db>        Clear database if flag is 1, don't clear otherwise"
     exit 1
 fi;
 
@@ -13,6 +14,7 @@ host=${1}
 thredds_url=${2//\//\\/}
 openid_rp_url=\"${3//\//\\/}\"
 if [ $4 == 1 ]; then debug=True; else debug=False; fi
+clearDB=$5
 
 echo copying config to ${host}...
 cd config
@@ -26,7 +28,10 @@ scp settings.py thomass@${host}:~/portal/middleware/portalflask/settings.py
 rm settings.py
 echo '...done. Re-creating caches...'
 ssh -t thomass@${host} '~/projects/OpEcVis_git/GISportal/clearcache'
-echo '...done. Re-creating database...'
-ssh -t thomass@${host} 'echo opec | sudo -S ~/projects/OpEcVis_git/GISportal/cleardb.sh'
+if [ ${clearDB} == 1 ];
+then
+    echo '...done. Re-creating database...'
+    ssh -t thomass@${host} 'echo opec | sudo -S ~/projects/OpEcVis_git/GISportal/cleardb.sh'
+fi
 cd ..
 echo '...done.'
